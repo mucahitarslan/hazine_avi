35210a3236ce3b35c23d1561bac56730a76f2802
mucahitarslan <arslanmucahit@outlook.com.tr>
2024-12-24 19:44:18 +0300

    sınıflar taslak olarak olusturuldu

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdlib> // Rastgele sayı üretimi için
+#include <ctime>
+
+using namespace std;
+
+class GameObject {
+protected:
+    
+public:
+
+};
+
+class Player : public GameObject {
+private:
+
+public:
+
+};
+
+class Treasure : public GameObject {
+public:
+
+};
+
+// Enemy sınıfı: Oyuncunun canını azaltan düşman nesnesi
+// GameObject sınıfından türetilmiştir (kalıtım).
+class Enemy : public GameObject {
+public:
+
+};
+
+
+void drawMap(int playerX, int playerY) {
+
+}
+
+int main(){
+
+    return 0;
+}
